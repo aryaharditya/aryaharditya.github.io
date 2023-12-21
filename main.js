43c4e0190e28d1ed87f32845501d6f6a9027adcd
aryaharditya <94812783+aryaharditya@users.noreply.github.com>
2023-12-21 15:09:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,82 +19,69 @@ function init() {
 
 		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
 
-				const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 3 );
-				light.position.set( 0.5, 1, 0.25 );
-				scene.add( light );
+		const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 3 );
+		light.position.set( 0.5, 1, 0.25 );
+		scene.add( light );
 
-				//
+		renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.xr.enabled = true;
+		container.appendChild( renderer.domElement );
 
-				renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.xr.enabled = true;
-				container.appendChild( renderer.domElement );
 
-				//
+		document.body.appendChild( ARButton.createButton( renderer ) );
 
-				document.body.appendChild( ARButton.createButton( renderer ) );
+		const loader = new GLTFLoader();
 
-				//
-				const loader = new GLTFLoader();
+		loader.load( 'Flamingo.glb', function ( gltf ) {
 
-				loader.load( 'Flamingo.glb', function ( gltf ) {
+		const mesh = gltf.scene.children[ 0 ];
 
-				const mesh = gltf.scene.children[ 0 ];
+		const s = 0.35;
+		mesh.scale.set( s, s, s );
+		mesh.position.y = 15;
+		mesh.rotation.y = - 1;
 
-					const s = 0.35;
-					mesh.scale.set( s, s, s );
-					mesh.position.y = 15;
-					mesh.rotation.y = - 1;
+		mesh.castShadow = true;
+		mesh.receiveShadow = true;
 
-					mesh.castShadow = true;
-					mesh.receiveShadow = true;
+		scene.add( mesh );
 
-					scene.add( mesh );
+		const mixer = new THREE.AnimationMixer( mesh );
+		mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
+		mixers.push( mixer );
 
-					const mixer = new THREE.AnimationMixer( mesh );
-					mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
-					mixers.push( mixer );
+	} );
 
-				} );
+	function onSelect() {
 
-				function onSelect() {
+		mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+		mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+		scene.add( mesh );
+	}
 
-					mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
-					mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-					scene.add( mesh );
+	controller = renderer.xr.getController( 0 );
+	controller.addEventListener( 'select', onSelect );
+	scene.add( controller );
 
-				}
+	window.addEventListener( 'resize', onWindowResize );
+}
 
-				controller = renderer.xr.getController( 0 );
-				controller.addEventListener( 'select', onSelect );
-				scene.add( controller );
+function onWindowResize() {
 
-				//
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-				window.addEventListener( 'resize', onWindowResize );
-
-			}
-
-			function onWindowResize() {
-
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			}
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 			//
 
-			function animate() {
+function animate() {
+	renderer.setAnimationLoop( render );
+}
 
-				renderer.setAnimationLoop( render );
-
-			}
-
-			function render() {
-
-				renderer.render( scene, camera );
-
-			}
+function render() {
+	renderer.render( scene, camera );
+}
